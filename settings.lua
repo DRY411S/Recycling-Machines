e1199a0900d1ffa211fcff7e81ddffdab7dd63aa
DRY411S <DRY411S@users.noreply.github.com>
2020-09-11 08:46:35 +0100

    v1.0.3
    11-Sep-2020
    
    Fix: https://github.com/DRY411S/Recycling-Machines/issues/91

--- settings.lua
+++ settings.lua
@@ -12,7 +12,10 @@ data:extend({
       name = "ZRecycling-recoveryspeed",
       setting_type = "startup",
       default_value = "1/2",
-	  allowed_values = { "1", "1/2", "1/3", "1/4" }, 
+	  -- Fix for https://github.com/DRY411S/Recycling-Machines/issues/91
+	  -- When setting craftspeed to 1 the mod fails to load
+	  -- The '=' setting had been changed to '1'. The comments say it should be '='
+	  allowed_values = { "=", "1/2", "1/3", "1/4" }, 
       order = "ba",
    },
 })
